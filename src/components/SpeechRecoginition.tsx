e15d1035984baf713d8cc28393e9fb29966bf787
hatimpatanwala <hatim@beatroute.io>
2023-03-18 10:45:32 +0530

    added ui filtering and speech recognition

--- src/components/SpeechRecoginition.tsx
+++ src/components/SpeechRecoginition.tsx
@@ -1,37 +1,140 @@
-import { IonContent, IonPage, IonButton } from '@ionic/react';
-import React, { useEffect, useState } from 'react';
+import {
+  IonContent,
+  IonPage,
+  IonButton,
+  IonSpinner,
+  IonGrid,
+  IonCol,
+  IonRow,
+  IonInput,
+  useIonToast,
+} from '@ionic/react';
+import React, { FormEvent, useEffect, useState } from 'react';
 import { SpeechRecognition } from '@capacitor-community/speech-recognition';
-
+import './SpeechRecognition.css';
+interface DataI {
+  item_name: string;
+  qty: number;
+  mrp: number;
+  rate: number;
+  cost: number;
+}
 const SpeechRecoginitionComponent: React.FC = () => {
   const [transscript, setTranssrcript] = useState<string>('');
   const [isSpeaking, setIsSpeaking] = useState<boolean>(false);
+  const [data, setData] = useState<DataI[] | null>(null);
+  const [filteredData, setFilterData] = useState<DataI[]>([]);
+  const [present] = useIonToast();
   useEffect(() => {
+    fetch('./assets/data.json')
+      .then((res) => res.json())
+      .then((result: DataI[]) => {
+        console.log(result);
+        setData(result);
+        setFilterData(result);
+      });
     SpeechRecognition.requestPermission();
   }, []);
-  const startRecognition = async (): Promise<void> => {
-    const available = await SpeechRecognition.available();
-    if (available) {
-      SpeechRecognition.start({
-        language: 'en-US',
-        maxResults: 2,
-        prompt: 'Say something',
-        partialResults: true,
-        popup: true,
-      });
-    }
-    SpeechRecognition.addListener('partialResults', (data) => {
-      console.log(data);
+  const presentToast = (
+    position: 'top' | 'middle' | 'bottom',
+    message: string
+  ) => {
+    present({
+      message: message,
+      duration: 2500,
+      position: position,
     });
   };
+  SpeechRecognition.addListener('partialResults', (result) => {
+    if (result.matches && result.matches.length > 0) {
+      filtering(result.matches[0]);
+    }
+    console.log(result);
+  });
+  const startRecognition = async (): Promise<void> => {
+    try {
+      const available = await SpeechRecognition.available();
+      if (available) {
+        setIsSpeaking(true);
+        SpeechRecognition.start({
+          language: 'en-US',
+          // maxResults: 2,
+          // prompt: 'Say something',
+          partialResults: true,
+          // popup: true,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      presentToast(
+        'top',
+        'Something went wrong while trying to listen either you havent given permission or trying on web'
+      );
+    }
+  };
   const StopRecognition = async (): Promise<void> => {
+    setIsSpeaking(false);
     await SpeechRecognition.stop();
-    setIsSpeaking(true);
+  };
+  const filtering = (e: any) => {
+    console.log(e);
+    // const result: any = data?.filter((obj) =>
+    //   Object.values(obj.item_name).some((val) => val.includes(e))
+    // );
+    // console.log(result);
+    const result1: any = data?.filter((obj) => {
+      if (obj.item_name.search(e.toUpperCase()) !== -1) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    console.log(result1);
+    setFilterData(result1);
   };
   return (
     <IonPage>
       <IonContent>
-        <IonButton onClick={startRecognition}>Start</IonButton>
-        <IonButton onClick={StopRecognition}>Stop</IonButton>
+        <div className='btn-container'>
+          {!isSpeaking ? (
+            <IonButton onClick={startRecognition}>Start</IonButton>
+          ) : (
+            <IonButton onClick={StopRecognition}>Stop</IonButton>
+          )}
+        </div>
+        {/* <IonInput placeholder='Enter text' onInput={filtering}></IonInput> */}
+        {isSpeaking && (
+          <div className='listen-spinner'>
+            I am listening
+            <IonSpinner name='dots'></IonSpinner>
+          </div>
+        )}
+        {transscript && <div>{transscript}</div>}
+        {data && (
+          <div className='table'>
+            <IonGrid>
+              <IonRow>
+                <IonCol>Item Name</IonCol>
+                <IonCol>Quantity</IonCol>
+                <IonCol>M.R.P</IonCol>
+                <IonCol>Cost</IonCol>
+                <IonCol>Rate</IonCol>
+              </IonRow>
+              {filteredData?.length &&
+                filteredData.map((d: DataI, index) => {
+                  return (
+                    <IonRow key={index}>
+                      <IonCol>{d.item_name}</IonCol>
+                      <IonCol>{d.qty}</IonCol>
+                      <IonCol>{d.mrp}</IonCol>
+                      <IonCol>{d.cost}</IonCol>
+                      <IonCol>{d.rate}</IonCol>
+                    </IonRow>
+                  );
+                })}
+            </IonGrid>
+          </div>
+        )}
       </IonContent>
     </IonPage>
   );
